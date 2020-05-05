389862fb9752ab5cb5b6b5d2f3ede89b24f9cfa3
globalcanlitv <53581316+globalcanlitv@users.noreply.github.com>
2020-05-05 20:34:37 +0200

    Cccc

---
+++ dene7777773.php
@@ -0,0 +1,47 @@
+<?php
+if(empty($_POST)) {
+echo "Kaybol git lan.";
+}
+else
+{
+?>
+{"X":[{"A":"0","B":"TÜM KATEGORİLER","C":"","D":"http://www.mobilcanlitvizle.com/kategorilogo/tumu.png","E":"0"},
+
+{"A":"14","B":"TRT EBA TV EĞİTİM","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/ulusal.png","E":"14"},
+
+{"A":"1","B":"ULUSAL KANALLAR","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/ulusal.png","E":"1"},
+
+{"A":"2","B":"BELGESEL KANALLARI","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/ulusal.png","E":"2"},
+
+{"A":"3","B":"SPOR KANALLARI","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/ulusal.png","E":"3"},
+
+{"A":"4","B":"HABER KANALLARI","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/ulusal.png","E":"4"},
+
+{"A":"5","B":"MÜZİK KANALLARI","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/ulusal.png","E":"5"},
+
+{"A":"6","B":"ÇOCUK KANALLARI","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/ulusal.png","E":"6"},
+
+{"A":"7","B":"SİNEMA KANALLARI","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/ulusal.png","E":"7"},
+
+{"A":"10","B":"YEREL KANALLAR","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/ulusal.png","E":"10"},
+
+{"A":"12","B":"DİNİ KANALLAR","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/ulusal.png","E":"12"},
+
+{"A":"8","B":"KURDİSH","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/haber.png","E":"8"},
+
+{"A":"11","B":"GERMANY","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/ulusal.png","E":"11"},
+
+{"A":"13","B":"AZERBAİJAN","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/ulusal.png","E":"13"},
+
+{"A":"9","B":"BELÇİKA/FRANSA","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/diger.png","E":"9"}],
+
+
+"Y":[{"A":"0","B":"İletişim & Hata Bildir","C":"1","D":"ULUSAL KANALLAR","E":"İletişim ve hata bildirmek için bizimle irtibat kurun.","F":"https://4.bp.blogspot.com/-0yHz4iZlBBs/XWKnF3sJ26I/AAAAAAAAAOI/MSicRpaMiqgwIvpylUT3BSmwr9mQ9rf8ACPcBGAYYCw/s1600/kjH7gkh.png","G":"https://iletisim93.blogspot.com/?m=1","H":"webplayer","I":"false","J":"direct","K":"","L":"","M":"","R":"","N":"","O":"03.05.2019 16:32:35","P":0},
+
+{"A":"03","B":"Telegram Grubumuz","C":"1","D":"ULUSAL KANALLAR","E":"Yeniliklerden ilk siz haberdar olun.","F":"https://1.bp.blogspot.com/-7vi-miz6tSc/XWseU6ipwbI/AAAAAAAAATc/qqvWZ8xswQ0YzCW4VCC4cBarOgE4aSIhwCLcBGAs/s1600/PicsArt_09-01-03.20.32.png","G":"https://t.me/joinchat/MNLF0w-V4XwQZJOSr6kzMw","H":"openbrowser","I":"false","J":"direct","K":"","L":"","M":"","R":"","N":"","O":"03.05.2019 16:32:35","P":0},
+
+
+
+
+{"A":"1132","B":"YENİ GÜNCELLEME","C":"9","D":"BELGİUM/FRANCE","E":"BELGİUM/FRANCE","F":"https://4.bp.blogspot.com/-HFAld771d3c/XWKnF4pmtrI/AAAAAAAAAN0/eOZbGDyUKG4Vzf_K-oHgDHoSggjbH0yxwCPcBGAYYCw/s1600/4zn11v2.png","G":"https://iptvpro93.github.io/download.html","H":"openbrowser","I":"false","J":"direct","K":"","L":"","M":"","R":"","N":"Mozilla\/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/55.0.2883.87 Safari\/537.36","O":"06.04.2019 11:23:30","P":0}]}
+<?php } ?>
